f8ffae2bad87216c375b0881b07658926ba5b98b
bangunhusada <farmasi.bangunhusada@gmail.com>
2025-09-08 13:27:02 +0700

    okkk

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-// Jika kamu tidak punya CSS, kamu boleh hapus baris ini
-// atau buat file kosong src/index.css
-// import "./index.css";
+// import "./index.css"; // <-- hapus atau pastikan filenya ada
 
-const rootEl = document.getElementById("root");
-if (rootEl) {
-  createRoot(rootEl).render(<App />);
-}
+const el = document.getElementById("root");
+if (el) createRoot(el).render(<App />);
