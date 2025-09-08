3ce9ad4ed5d6c5269bd029f7372db34c9d822df9
bangunhusada <farmasi.bangunhusada@gmail.com>
2025-09-08 11:19:42 +0700

    ok

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(<App />)
+const container = document.getElementById("root")!;
+createRoot(container).render(<App />);
