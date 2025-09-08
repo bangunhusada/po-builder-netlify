5b70b0e38329bbd7f5d8a689f37145827ab49a19
bangunhusada <farmasi.bangunhusada@gmail.com>
2025-09-08 13:30:11 +0700

    cape

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-// import "./index.css"; // <-- hapus atau pastikan filenya ada
+import "./index.css";  // <— WAJIB ada
 
 const el = document.getElementById("root");
 if (el) createRoot(el).render(<App />);
