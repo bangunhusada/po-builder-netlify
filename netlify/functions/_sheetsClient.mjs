7c20a22612fd6344c586216740ee8125068992bf
bangunhusada <farmasi.bangunhusada@gmail.com>
2025-09-09 08:02:22 +0700

    Update _sheetsClient.mjs

--- netlify/functions/_sheetsClient.mjs
+++ netlify/functions/_sheetsClient.mjs
@@ -1,23 +1,20 @@
 // netlify/functions/_sheetsClient.mjs
 import { google } from "googleapis";
 
-export async function getSheets() {
-  const clientEmail = process.env.GCP_CLIENT_EMAIL;
+export function getSheets() {
+  const clientEmail = process.env.GCP_CLIENT_EMAIL || "";
   let privateKey = process.env.GCP_PRIVATE_KEY || "";
+  privateKey = privateKey.replace(/\\n/g, "\n"); // handle \n
 
   if (!clientEmail || !privateKey) {
-    throw new Error("Missing env: GCP_CLIENT_EMAIL or GCP_PRIVATE_KEY");
+    throw new Error("Missing GCP_CLIENT_EMAIL / GCP_PRIVATE_KEY");
   }
 
-  // Netlify menyimpan newline sebagai \n -> kembalikan ke newline asli
-  privateKey = privateKey.replace(/\\n/g, "\n");
-
-  const jwt = new google.auth.JWT({
+  const auth = new google.auth.JWT({
     email: clientEmail,
     key: privateKey,
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
   });
 
-  await jwt.authorize();
-  return google.sheets({ version: "v4", auth: jwt });
+  return google.sheets({ version: "v4", auth });
 }
